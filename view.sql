c2fb845de228aba5aab976bfb07dfd201a9d49ac
cruzguo <155496535+cruzguo@users.noreply.github.com>
2024-07-20 19:57:25 +0200

    updated view. First pass with no testing done yet.

--- view.sql
+++ view.sql
@@ -2,24 +2,21 @@ create or replace view flights_in_the_air as
 select flight.flightID as flightID, leg.departure as departure, 
 leg.arrival as arrival, flight.next_time as arrival_time, 
 (support_airline, support_tail) as airplane, 
-count(*) as total_airport_flights
+count(*) over (partition by leg.departure, leg.arrival) as total_airport_flights
 from airplane join flight on (support_airline, support_tail) = (airlineID, tail_num) 
 join route_path on flight.routeID = route_path.routeID
 join leg  on route_path.legID = leg.legID
-where locID is null
-group by (departure, arrival);
+where locID is null;
 
 create or replace view flights_on_the_ground as
 select flight.flightID as flightID, airport.city as departure_city, 
 (airlineID, tail_num) as airplane, 
-count(*) as total_flights_from_city
+count(*) over (partition by airport.city) as total_flights_from_city
 from flight join airplane on (support_airline, support_tail) = (airlineID, tail_num)
-join airport on airplane.locID = airport.locID
-group by airport.city;
+join airport on airplane.locID = airport.locID;
 
 create or replace view alternative_airports as 
 select airport.city as city, airport.state as state,  
-count(*) as number_of_airports,
+count(*) over (partition by airport.city, airport.state) as number_of_airports,
 airport.airportID as airportID, airport.name as name
-from airport
-group by (airport.city, airport.state);
+from airport;
