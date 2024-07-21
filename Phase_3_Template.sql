2038f1565a5f9ee26526d0a9b8b51cd62820a634
Sam Heseltine <bills2021e@gmail.com>
2024-07-22 00:14:48 +0300

    Add procedures to template file

--- Phase_3_Template.sql
+++ Phase_3_Template.sql
@@ -35,6 +35,7 @@ begin
     set hours = truncate(total_time, 0);
     set minutes = truncate((total_time - hours) * 60, 0);
     return maketime(hours, minutes, 0);
+
 end //
 delimiter ;
 
@@ -54,6 +55,30 @@ create procedure add_airplane (in ip_airlineID varchar(50), in ip_tail_num varch
     in ip_plane_type varchar(100), in ip_skids boolean, in ip_propellers integer,
     in ip_jet_engines integer)
 sp_main: begin
+    declare airline_count int;
+    declare location_count int;
+
+    select count(*) into airline_count
+    from airline
+    where airlineID = ip_airlineID;
+
+    if airline_count = 0 then
+        leave sp_main;
+    end if;
+
+    select count(*) int location_count
+    from location
+    where locID = ip_locationID;
+
+    if location_count > 0 then
+        leave sp_main;
+    end if;
+
+    insert into airplane (airlineID, tail_num, seat_cap, speed, locID, plane_type, skids, props, engine)
+    values (ip_airlineID, ip_tail_num, ip_seat_capacity, ip_speed, ip_locationID, ip_plane_type, ip_skids, ip_propellers, ip_jet_engines);
+
+    insert into location (locID)
+    values (ip_locationID);
 
 end //
 delimiter ;
@@ -70,6 +95,21 @@ delimiter //
 create procedure add_airport (in ip_airportID char(3), in ip_airport_name varchar(200),
     in ip_city varchar(100), in ip_state varchar(100), in ip_country char(3), in ip_locationID varchar(50))
 sp_main: begin
+    declare location_count int;
+
+    select count(*) into location_count
+    from location
+    where locID = ip_locationID;
+
+    if location_count > 0 then
+        leave sp_main;
+    end if;
+
+    insert into airport (airportID, name, city, state, country, locID)
+    values (ip_airportID, ip_airport_name, ip_city, ip_state, ip_country, ip_locationID);
+
+    insert into location (locID)
+    values (ip_locationID);
 
 end //
 delimiter ;
@@ -90,6 +130,27 @@ create procedure offer_flight (in ip_flightID varchar(50), in ip_routeID varchar
     in ip_support_airline varchar(50), in ip_support_tail varchar(50), in ip_progress integer,
     in ip_next_time time, in ip_cost integer)
 sp_main: begin
+    declare route_count int;
+    declare airplane_count int;
+
+    select count(*) into route_count
+    from route
+    where routeID = ip_routeID;
+
+    if route_count = 0 then
+        leave sp_main;
+    end if;
+
+    select count(*) into airplane_count
+    from flight
+    where support_airline = ip_support_airline and support_tail = ip_support_tail and status = 'in_flight';
+
+    if airplane_count > 0 then
+        leave sp_main;
+    end if;
+
+    insert into flight (flightID, routeID, support_airline, support_tail, progress, status, next_time, cost)
+    values (ip_flightID, ip_routeID, ip_support_airline, ip_support_tail, ip_progress, 'on_ground', ip_next_time, ip_cost);
 
 end //
 delimiter ;
@@ -99,12 +160,37 @@ delimiter ;
 /* This stored procedure updates the state for a flight landing at the next airport
 along it's route.  The time for the flight should be moved one hour into the future
 to allow for the flight to be checked, refueled, restocked, etc. for the next leg
-of travel. 
+of travel. */
 -- -----------------------------------------------------------------------------
 drop procedure if exists flight_landing;
 delimiter //
 create procedure flight_landing (in ip_flightID varchar(50))
 sp_main: begin
+    declare _routeID int;
+    declare _progress int;
+    declare _leg_count int;
+    declare _current_time time;
+    declare _new_time time;
+
+    select routeID, progress, next_time into _routeID, _progress, _current_time
+    from flight
+    where flightID = ip_flightID;
+
+    select count(*) from _leg_count
+    from route_path
+    where routeID = _routeID;
+
+    if progress >= _leg_count then
+        signal sqlstate '45000'
+        set message_text = 'Flight has already completed its route'
+        leave sp_main;
+    end if;
+
+    update flight
+    set progress = _progress + 1,
+        status = 'on_ground',
+        next_time = addtime(_current_time, '01:00:00')
+    where flightID = ip_flightID;
 
 end //
 delimiter ;
@@ -120,6 +206,28 @@ drop procedure if exists retire_flight;
 delimiter //
 create procedure retire_flight (in ip_flightID varchar(50))
 sp_main: begin
+    declare _status enum('on_ground', 'in_flight');
+    declare _progress int;
+    declare _routeID int;
+    declare _leg_count int;
 
+    select status, progress, routeID into _status, _progress, _routeID
+    from flight
+    where flightID = ip_flightID;
+
+    if _status != 'on_ground' then
+        leave sp_main;
+    end if;
+
+    select count(*) into _leg_count
+    from route_path
+    where routeID = _routeID;
+
+    if _progress != 0 and _progress != _leg_count then
+        leave sp_main;
+    end if;
+
+    delete from flight
+    where flightID = ip_flightID;
 end //
 delimiter ;
--- create.sql
+++ create.sql
@@ -19,78 +19,78 @@ use SAMS;
 
 
 create table airline (
-airlineID varchar(50),
-revenue decimal(50, 2) default 0,
-primary key (airlineID)
+	airlineID varchar(50),
+	revenue decimal(50, 2) default 0,
+	primary key (airlineID)
 );
 
 create table location (
-locID varchar(50),
-primary key (locID)
+	locID varchar(50),
+	primary key (locID)
 );
 
 create table airplane (
-airlineID varchar(50),
-tail_num varchar(50),
-seat_cap int,
-speed int, -- mph
-locID varchar(50), -- null indicates it is in flight
-plane_type varchar(100) not null default 'other', -- jet, propeller, other
-skids boolean default False, -- should only be true if propeller type
-props int default null, -- number of propellers for propeller type -- null if not propeller type
-engines int default null, -- number of engines for jet type -- null if not jet type
-primary key (airlineID, tail_num),
-constraint fk_airplane_airlineID foreign key (airlineID) references airline(airlineID) on delete cascade on update cascade, 
-constraint fk_airplane_locID foreign key (locID) references location(locID) on delete cascade on update cascade
+	airlineID varchar(50),
+	tail_num varchar(50),
+	seat_cap int,
+	speed int, -- mph
+	locID varchar(50), -- null indicates it is in flight
+	plane_type varchar(100) not null default 'other', -- jet, propeller, other
+	skids boolean default false, -- should only be true if propeller type
+	props int default null, -- number of propellers for propeller type -- null if not propeller type
+	engines int default null, -- number of engines for jet type -- null if not jet type
+	primary key (airlineID, tail_num),
+	constraint fk_airplane_airlineID foreign key (airlineID) references airline(airlineID) on delete cascade on update cascade, 
+	constraint fk_airplane_locID foreign key (locID) references location(locID) on delete cascade on update cascade
 );
 
 create table airport (
-airportID varchar(50),
-name varchar (200) not null,
-city varchar(100),
-state varchar(100),
-country varchar(100),
-locID varchar(50) not null,
-primary key (airportID),
-constraint fk_airport_locID foreign key (locID) references location(locID) on delete cascade on update cascade
+	airportID varchar(50),
+	name varchar (200) not null,
+	city varchar(100),
+	state varchar(100),
+	country varchar(100),
+	locID varchar(50) not null,
+	primary key (airportID),
+	constraint fk_airport_locID foreign key (locID) references location(locID) on delete cascade on update cascade
 );
 
 create table leg (
-legID int,
-distance int default 0,
-departure varchar(50) not null,
-arrival varchar(50) not null,
-primary key (legID),
-constraint fk_leg_departure foreign key (departure) references airport(airportID) on delete cascade on update cascade,
-constraint fk_leg_arrival foreign key (arrival) references airport(airportID) on delete cascade on update cascade
+	legID int,
+	distance int default 0,
+	departure varchar(50) not null,
+	arrival varchar(50) not null,
+	primary key (legID),
+	constraint fk_leg_departure foreign key (departure) references airport(airportID) on delete cascade on update cascade,
+	constraint fk_leg_arrival foreign key (arrival) references airport(airportID) on delete cascade on update cascade
 );
 
 create table route (
-routeID int,
-primary key(routeID)
+	routeID int,
+	primary key(routeID)
 );
 
 create table route_path (
-routeID int,
-legID int,
-sequence int, -- WTF
-primary key (routeID, legID, sequence),  -- Not sure what modifying the primary key to prioritize the sequence/ordering attribute
-constraint fk_route_path_routeID foreign key (routeID) references route(routeID) on delete cascade on update cascade,
-constraint fk_route_path_legID foreign key (legID) references leg(legID) on delete cascade on update cascade
+	routeID int,
+	legID int,
+	sequence int, -- WTF
+	primary key (routeID, legID, sequence),  -- Not sure what modifying the primary key to prioritize the sequence/ordering attribute
+	constraint fk_route_path_routeID foreign key (routeID) references route(routeID) on delete cascade on update cascade,
+	constraint fk_route_path_legID foreign key (legID) references leg(legID) on delete cascade on update cascade
 );
 
 create table flight (
-flightID varchar(50),
-routeID int not null,
-support_airline varchar(50),
-support_tail varchar(50),
-progress int default 0,
-status enum('on_ground', 'in_flight') default 'on_ground',
-next_time time,
-cost decimal (50, 2) default 0,
-primary key (flightID),
-constraint fk_flight_routeID foreign key (routeID) references route(routeID) on delete cascade on update cascade,
-constraint fk_flight_support foreign key (support_airline, support_tail) references airplane(airlineID, tail_num) on delete cascade on update cascade
+	flightID varchar(50),
+	routeID int not null,
+	support_airline varchar(50),
+	support_tail varchar(50),
+	progress int default 0,
+	status enum('on_ground', 'in_flight') default 'on_ground',
+	next_time time,
+	cost decimal (50, 2) default 0,
+	primary key (flightID),
+	constraint fk_flight_routeID foreign key (routeID) references route(routeID) on delete cascade on update cascade,
+	constraint fk_flight_support foreign key (support_airline, support_tail) references airplane(airlineID, tail_num) on delete cascade on update cascade
 );
 
 delimiter //
