a5bf30596448d214a040b021fbb1cdb66fa7125e
cruzguo <155496535+cruzguo@users.noreply.github.com>
2024-07-22 17:46:55 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -19,7 +19,7 @@ insert into airplane (airlineID, tail_num, seat_cap, speed, locID, plane_type, p
 ('DL', 'N123DL', 150, 560, 'LOC1', 'jet', 0, 2),
 ('DL', 'N456DL', 180, 580, 'LOC2', 'jet', 0, 2),
 ('SW', 'N789SW', 140, 500, 'LOC3', 'jet', 0, 2),
-('UA', 'N101UA', 160, 550, 'LOC1', 'jet', 0, 2),
+('UA', 'N101UA', 160, 550, 'LOC2', 'jet', 0, 2),
 ('UA', 'N202UA', 200, 600, 'LOC2', 'jet', 0, 2);
 
 insert into leg (legID, distance, departure, arrival) values
@@ -40,7 +40,7 @@ insert into route_path (routeID, legID, sequence) values
 insert into flight (flightID, routeID, support_airline, support_tail, progress, status, next_time, cost) values
 ('FL001', 'BWI-ATL-JFK', 'DL', 'N123DL', 1, 'on_ground', '14:00:00', 500.00),
 ('FL002', 'BWI-ATL-JFK', 'DL', 'N456DL', 2, 'on_ground', '15:00:00', 600.00),
-('FL003', 'BWI-JFK', 'UA', 'N101UA', 1, 'in_flight', '16:00:00', 550.00);
+('FL003', 'BWI-JFK', 'UA', 'N101UA', 1, 'on_ground', '16:00:00', 550.00);
 
 -- insert stuff below
 insert into airline values
@@ -60,12 +60,12 @@ insert into location values
 insert into airplane values
 ('DL', 'N789DL', 150, 717, 'LOC5', 'jet', False, 0, 20),
 ('AA', 'N111AA', 300, 300, null, 'big helicopter', False, 1, 2),
-('BB', 'N111BB', 211, 1589, 'LOC8', 'jet', False, 0, 40),
+('BB', 'N111BB', 211, 1589, 'LOC2', 'jet', False, 0, 40),
 ('UA', 'N301UA', 10, 269, 'LOC7', 'propeller', True, 3, 0),
-('CC', 'N111CC', 709, 678, null, 'jet', False, 0, 62);
+('CC', 'N111CC', 709, 678, 'LOC8', 'jet', False, 0, 62);
 
 insert into airport values
-('BCA', 'Big Chungus Airport', 'Accident', 'Maryland', 'USA', 'LOC7'),
+('BCA', 'Big Chungus Airport', 'Atlanta', 'Georgia', 'USA', 'LOC2'),
 ('AAA', 'Amogus Airport', 'Bacon Level', 'Alabama', 'USA', 'LOC5'),
 ('TTA', 'Tilted Towers Airport', 'Batman', 'Batman', 'Turkey', 'LOC6'),
 ('RO', 'Residencia Onix', 'Barcelona', 'Cataluyna', 'Spain', 'LOC4'),
@@ -83,18 +83,21 @@ insert into route values
 ('RO-GAN'),
 ('JFK-ATL'),
 ('AAA-TTA-GAN'),
-('GAN-BWI-BCA');
+('GAN-BWI-BCA'),
+('BCA-BWI-JFK'),
+('BCA-BWI');
 
 insert into route_path values
 ('JFK-ATL', 12111, 69),
 ('BCA-AAA-BWI', 78, 1),
-('BCA-AAA-BWI', 17, 2),
-('AAA-TTA-GAN', 2233, 1), -- purposefully placed a non-existing route for a leg
-('RO-GAN', 900, 1);
+('BCA-BWI', 17, 2),
+('AAA-TTA-GAN', 2233, 1),
+('RO-GAN', 900, 1),
+('BCA-BWI-JFK', 4, 2);
 
 insert into flight values
 ('FL008', 'BCA-AAA-BWI', 'AA', 'N111AA', 1, 'in_flight', '05:22:17', 1.50),
-('FL004', 'RO-GAN', 'BB', 'N111BB', 0, 'in_flight', '12:57:10', 982.22),
+('FL004', 'BCA-BWI-JFK', 'BB', 'N111BB', 1, 'on_ground', '12:57:10', 982.22),
 ('FL005', 'JFK-ATL', 'CC', 'N111CC', 2, 'on_ground', '13:33:00', 2000.00),
 ('FL006', 'AAA-TTA-GAN', 'DL', 'N789DL', 1, 'on_ground', '22:01:22', 100.50),
 ('FL007', 'GAN-BWI-BCA', 'UA', 'N301UA', 3, 'on_ground', '20:12:09', 4440.10);
